d8a9eae0a052fd656c87238acc0766f15ae410c2
Ali Zartash <ali.zartash@gmail.com>
2024-09-19 01:45:00 -0700

    first commit

---
+++ setup_new_linux.sh
@@ -0,0 +1,16 @@
+# setup of a new system [MacOS, Linux]
+#
+# set up github access keys and clone this repo to ~/
+# Install powerline patched Monaco font on system
+# change terminal emulator font to  Monaco for Powerline
+# Install oh-my-zsh, change default shell to ZSH
+# install zsh-vi-mode plugin:
+#   git clone https://github.com/jeffreytse/zsh-vi-mode \
+#   $ZSH_CUSTOM/plugins/zsh-vi-mode
+# Then enable this plugin by adding to plugins in .zshrc
+# change oh-my-zsh theme to `agnoster`
+# install tmux
+# link ~/.tmux.config to this repo's version
+# link ~/.vimrc to this repo's version
+# link ~/.vim/colors to this repo's version
+# source this repo's zshrc in the actual .zshrc
